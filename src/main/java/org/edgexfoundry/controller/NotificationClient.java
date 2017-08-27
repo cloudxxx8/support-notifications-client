9c62f88e2e5514da2ae64e50bc057b1efd1c8526
Jim White <james_white2@dell.com>
2017-08-27 15:13:03 -0500

    add checkstyle to pom, added tests, clean up of code and tests
    Signed-off-by: Jim White <james_white2@dell.com>

--- src/main/java/org/edgexfoundry/controller/NotificationClient.java
+++ src/main/java/org/edgexfoundry/controller/NotificationClient.java
@@ -1,22 +1,21 @@
 /*******************************************************************************
  * Copyright 2016-2017 Dell Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
- * @microservice:  support-notifications-client library
+ * @microservice: support-notifications-client library
  * @author: Cloud Tsai, Dell
  * @version: 1.0.0
  *******************************************************************************/
+
 package org.edgexfoundry.controller;
 
 import java.util.List;
@@ -32,49 +31,52 @@ import javax.ws.rs.core.MediaType;
 
 import org.edgexfoundry.support.domain.notifications.Notification;
 
+// TODO - put in org-edgexfoundry-support-notifications-client package
 public interface NotificationClient {
 
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	String receiveNotification(Notification notification);
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  String receiveNotification(Notification notification);
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/slug/{slug:.+}")
-	Notification findBySlug(@PathParam("slug") String slug);
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/slug/{slug:.+}")
+  Notification findBySlug(@PathParam("slug") String slug);
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/sender/{sender:.+}/{limit}")
-	List<Notification> findBySender(@PathParam("sender") String sender, @PathParam("limit") int limit);
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/sender/{sender:.+}/{limit}")
+  List<Notification> findBySender(@PathParam("sender") String sender,
+      @PathParam("limit") int limit);
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{start}/{end}/{limit}")
-	List<Notification> findByCreatedDuration(@PathParam("start") long start, @PathParam("end") long end,
-			@PathParam("limit") int limit);
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/{start}/{end}/{limit}")
+  List<Notification> findByCreatedDuration(@PathParam("start") long start,
+      @PathParam("end") long end, @PathParam("limit") int limit);
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/start/{start}/{limit}")
-	List<Notification> findByCreatedAfter(@PathParam("start") long start, @PathParam("limit") int limit);
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/end/{end}/{limit}")
-	List<Notification> findByCreatedBefore(@PathParam("end") long end, @PathParam("limit") int limit);
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/new/{limit}")
-	List<Notification> findNewNotifications(@PathParam("limit") int limit);
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/start/{start}/{limit}")
+  List<Notification> findByCreatedAfter(@PathParam("start") long start,
+      @PathParam("limit") int limit);
 
-	@DELETE
-	@Path("/slug/{slug:.+}")
-	boolean deleteBySlug(@PathParam("slug") String slug);
-	
-	@DELETE
-	@Path("/age/{age}")
-	boolean deleteOld(@PathParam("age") long age);
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/end/{end}/{limit}")
+  List<Notification> findByCreatedBefore(@PathParam("end") long end, @PathParam("limit") int limit);
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/new/{limit}")
+  List<Notification> findNewNotifications(@PathParam("limit") int limit);
+
+  @DELETE
+  @Path("/slug/{slug:.+}")
+  boolean deleteBySlug(@PathParam("slug") String slug);
+
+  @DELETE
+  @Path("/age/{age}")
+  boolean deleteOld(@PathParam("age") long age);
 
 }
--- src/main/java/org/edgexfoundry/controller/SubscriptionClient.java
+++ src/main/java/org/edgexfoundry/controller/SubscriptionClient.java
@@ -1,22 +1,21 @@
 /*******************************************************************************
  * Copyright 2016-2017 Dell Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
- * @microservice:  support-notifications-client library
+ * @microservice: support-notifications-client library
  * @author: Cloud Tsai, Dell
  * @version: 1.0.0
  *******************************************************************************/
+
 package org.edgexfoundry.controller;
 
 import java.util.List;
@@ -33,32 +32,33 @@ import javax.ws.rs.core.MediaType;
 
 import org.edgexfoundry.support.domain.notifications.Subscription;
 
+// TODO - put in org-edgexfoundry-support-notifications-client package
 public interface SubscriptionClient {
-	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	String createSubscription(Subscription subscription);
-	
-	@PUT
-	@Consumes(MediaType.APPLICATION_JSON)
-	boolean updateSubscription(Subscription subscription);
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	List<Subscription> listAll();
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/slug/{slug:.+}")
-	Subscription findBySlug(@PathParam("slug") String slug);
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/receiver/{receiver:.+}")
-	List<Subscription> searchByReceiver(@PathParam("receiver") String receiver);
-	
-	@DELETE
-	@Path("/slug/{slug:.+}")
-	boolean deleteBySlug(@PathParam("slug") String slug);
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  String createSubscription(Subscription subscription);
+
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  boolean updateSubscription(Subscription subscription);
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  List<Subscription> listAll();
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/slug/{slug:.+}")
+  Subscription findBySlug(@PathParam("slug") String slug);
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/receiver/{receiver:.+}")
+  List<Subscription> searchByReceiver(@PathParam("receiver") String receiver);
+
+  @DELETE
+  @Path("/slug/{slug:.+}")
+  boolean deleteBySlug(@PathParam("slug") String slug);
 
 }
